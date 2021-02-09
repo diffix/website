fbe874c0b6ddb51d71b159b92ac64be02a60a583
Sebastian Probst Eide <sebastian@aircloak.com>
2021-02-09 16:25:49 +0100

    Add basic first page

---
+++ components/Terminal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-white transition-all bg-gray-900 rounded-lg shadow-lg dark:bg-shamrock-800 hover:bg-gray-800 w-96">
+    <div class="flex px-2 pt-2 pb-2 border-b border-gray-700">
+      <div class="w-3 h-3 bg-red-500 rounded-full hover:bg-red-400"></div>
+      <div class="w-3 h-3 ml-1 bg-yellow-500 rounded-full hover:bg-yellow-400"></div>
+      <div class="w-3 h-3 ml-1 bg-green-500 rounded-full hover:bg-green-400"></div>
+    </div>
+    <div class="p-2 font-mono text-gray-300">
+      <pre>
+$ psql
+
+# SELECT zip_code, count(*)
+  FROM country
+  GROUP BY zip_code;
+
+ zip_code | count
+----------+-------
+    10119 |   101
+    67541 |    24
+    80539 |  1911
+(3 rows)
+      </pre>
+      # <span class="blink">_</span>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.blink {
+  animation: blinker 1s linear infinite;
+}
+
+@keyframes blinker {
+  50% {
+    opacity: 0;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,78 +1,42 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        open-diffix
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="flex flex-col h-screen dark:text-gray-300">
+    <div class="flex-grow w-full">
+      <header class="max-w-2xl pt-20 mx-auto transition-all duration-500 lg:pt-60">
+        <h1 class="block font-bold text-center text-white transition-all duration-500 text-8xl w-100 md:text-9xl">
+          <span class="text-transparent bg-gradient-to-r bg-clip-text from-pink-500 to-purple-500 md:from-shamrock-500 md:to-royalblue-500">
+            open-diffix
+          </span>
+        </h1>
+        <h2 class="mx-12 mt-4 text-2xl text-center">
+          Free and Open Dynamic Anonymization for PostgreSQL
+        </h2>
+      </header>
+
+      <div class="max-w-4xl px-4 mx-auto mt-20 text-lg lg:flex">
+        <div class="flex-col justify-center max-w-lg mx-auto lg:w-1/2 lg:px-16 lg:pb-16 lg:flex ">
+          <p>
+            The Open Diffix project aims to bring world-class dynamic anonymization
+            to your software project, and is free to use for most use cases.
+          </p>
+          <p class="mt-4 font-semibold">
+            It's Postgres as you know it - only anonymous.
+          </p>
+        </div>
+        <Terminal class="mx-auto mt-8 text-base lg:mt-0 lg:mx-0 lg:w-1/2" />
+      </div>
+
+      <div>
       </div>
     </div>
+
+    <footer class="mx-auto my-10 text-sm text-gray-500">
+      Copyright MPI-SWS for Software Systems |
+      <a
+        href="https://imprint.mpi-klsb.mpg.de/sws/www.mpi-sws.org/"
+        class="text-gray-800 underline dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-300"
+      >
+        Imprint
+      </a>
+    </footer>
   </div>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
