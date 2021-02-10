59caddf4fd5132321c89be62339a4e9eec6fd6f4
Sebastian Probst Eide <sebastian@aircloak.com>
2021-02-10 16:35:36 +0100

    Hopefully no more double scrollbar in Firefox

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-screen h-screen overflow-y-auto dark:bg-gray-900 dark:text-gray-300">
+  <div class="min-h-screen dark:text-gray-300">
     <Nuxt />
   </div>
 </template>
