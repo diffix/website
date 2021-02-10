47f3ee44c84e79ce70c33d9e390461f15372d47d
Sebastian Probst Eide <sebastian@aircloak.com>
2021-02-10 11:48:53 +0100

    Temporary fix for nuxt related bug
    - https://github.com/nuxt-community/tailwindcss-module/issues/79#issuecomment-609693459
    - https://github.com/tailwindlabs/tailwindcss/discussions/2462

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,5 +35,14 @@ export default {
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          // Fixes: https://github.com/tailwindcss/tailwindcss/issues/1190#issuecomment-546621554
+          "focus-within-pseudo-class": false
+        }
+      }
+    }
+  }
 };
