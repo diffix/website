f180dafa451c260018d53d1d7f780ca708ce21b6
Sebastian Probst Eide <sebastian@aircloak.com>
2021-02-10 09:03:23 +0100

    Make terminal dual colour

--- components/Terminal.vue
+++ components/Terminal.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-full max-w-sm text-white transition-all duration-500 bg-gray-900 rounded-lg shadow-lg max-w-96 dark:hover:bg-shamrock-900 dark:bg-shamrock-800 hover:bg-gray-800">
-    <div class="flex px-2 pt-2 pb-2 border-b border-gray-700">
+  <div class="w-full max-w-sm text-white bg-gray-900 rounded-lg shadow-lg max-w-96 dark:bg-shamrock-800">
+    <div class="flex px-2 pt-2 pb-2 transition-colors border-b border-gray-700">
       <div class="w-3 h-3 bg-red-500 rounded-full hover:bg-red-400"></div>
       <div class="w-3 h-3 ml-1 bg-yellow-500 rounded-full hover:bg-yellow-400"></div>
       <div class="w-3 h-3 ml-1 bg-green-500 rounded-full hover:bg-green-400"></div>
     </div>
-    <div class="p-2 font-mono text-gray-300">
+    <div class="p-2 font-mono text-gray-300 bg-gray-800 dark:bg-shamrock-900">
       <pre>
 $ psql
 
