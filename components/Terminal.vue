5e5cab2d908144349b6133123fb63738100b8082
Sebastian Probst Eide <sebastian@aircloak.com>
2021-02-15 15:04:31 +0100

    Dynamic stupid type writer effect

--- components/Terminal.vue
+++ components/Terminal.vue
@@ -5,10 +5,16 @@
       <div class="w-3 h-3 ml-1 bg-yellow-500 rounded-full hover:bg-yellow-400"></div>
       <div class="w-3 h-3 ml-1 bg-green-500 rounded-full hover:bg-green-400"></div>
     </div>
-    <div class="p-2 font-mono text-gray-300 bg-gray-800 dark:bg-shamrock-900">
-      <pre>$ psql</pre>
-      <pre>{{ query }}</pre>
-      # <span class="blink">_</span>
+    <div class="h-full p-2 font-mono text-gray-300 bg-gray-800 dark:bg-shamrock-900">
+      <pre>
+$ psql
+
+# SELECT {{ query }}
+      </pre>
+
+      <span v-if="this.queryToUse === ''">
+        # <span class="blink">_</span>
+      </span>
     </div>
   </div>
 </template>
@@ -27,45 +33,71 @@
 </style>
 
 <script>
+function randomwShortWait() {
+  return 100 + 100 * Math.floor(Math.random() * 2);
+}
+
 export default {
   data: function () {
     const queries = [
-      `
-# SELECT count(dogs)
+      {
+        query: `count(dogs)
   FROM dalmatians;
-
- count
--------
-   103
-(1 row)
-      `, `
-# SELECT count(samurai)
-  FROM kurosawa_movies;
-
- count
--------
-     6
-(1 row)
-      `, `
-# SELECT count(soldiers)
-  FROM movie_dirty_dozen;
-
- count
--------
-    11
-(1 row)
-      `, `
-# SELECT count(citizens)
-  FROM citizen_kane;
-
- count
--------
-  null
-(1 row)
-      `
+        `,
+        answer: "   103"
+      },
+      {
+        query: `count(samurai)
+  FROM kurosawa_movies`,
+        answer: "     6"
+      },
+      {
+        query: `count(soldiers)
+  FROM movie_dirty_dozen`,
+        answer: "    11"
+      },
+      {
+        query: `count(citizens)
+  FROM citizen_kane`,
+        answer: "  null"
+      }
     ];
-    const query = queries[Math.floor(Math.random() * queries.length)];
-    return {query: query}
+    const toUse = queries[Math.floor(Math.random() * queries.length)];
+    return {
+      queryToUse: toUse.query,
+      answer: toUse.answer,
+      query: ""
+    }
+  },
+  methods: {
+    addNextCharcter() {
+      if (this.queryToUse.length > 0) {
+        this.query = this.query + this.queryToUse.substr(0, 1);
+        if (this.queryToUse.length > 1) {
+          this.queryToUse = this.queryToUse.substr(1, this.queryToUse.length);
+        } else {
+          this.query = this.query + ";"
+          this.queryToUse = "";
+        }
+        setTimeout(this.addNextCharcter, randomwShortWait());
+      } else {
+        setTimeout(this.addResult(), 2000);
+      }
+    },
+
+    addResult() {
+      let queryAnswer = `
+
+count
+-------\n` + this.answer + `
+(1 row)`;
+      this.query = this.query + queryAnswer;
+    },
+
+  },
+
+  mounted: function () {
+    setTimeout(this.addNextCharcter, 100);
   }
 }
 </script>
