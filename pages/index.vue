6394e14c0a3bc2fbaf36b1b146af43fee132a5ae
Sebastian Probst Eide <sebastian@aircloak.com>
2021-03-01 13:02:03 +0100

    Move mailinglist to separate page

--- pages/index.vue
+++ pages/index.vue
@@ -26,21 +26,21 @@
 
       <div class="flex flex-col items-center max-w-xl px-4 mx-auto mt-32 space-y-4 text-lg">
         <span>Want to stay informed about the development of <OpenDiffixWordmark />?</span>
-        <NuxtLink to="/newsletter" class="inline-block px-4 py-2 text-2xl text-center text-white transition-all duration-500 rounded-lg active:outline-none active:ring-2 hover:shadow hover:translate-x-12 bg-shamrock-500 hover:bg-gradient-to-r from-pink-500 to-purple-500 md:from-shamrock-500 md:to-royalblue-500">Sign up to our newsletter</NuxtLink>
+        <NuxtLink to="/newsletter" class="inline-block px-4 py-2 text-2xl text-center text-white transition-all duration-500 rounded-lg hover:shadow hover:translate-x-12 bg-shamrock-500 hover:bg-gradient-to-r from-pink-500 to-purple-500 md:from-shamrock-500 md:to-royalblue-500">Sign up to our newsletter</NuxtLink>
       </div>
 
-      <div class="flex-grow py-32 mt-32 bg-gray-50">
+      <div class="flex-grow pt-16 pb-16 mt-32 dark:pb-4 sm:pt-32 sm:pb-32 bg-gray-50 dark:bg-gray-800">
         <div class="max-w-xl px-4 mx-auto xl:max-w-5xl">
           <h2 class="text-xl text-center text-gray-500">
             <OpenDiffixWordmark />
             is sponsored by
           </h2>
 
-          <div class="flex flex-col items-center max-w-4xl mx-auto mt-12 space-y-8 xl:space-y-0 xl:mt-8 xl:items-end justify-items-start xl:flex-none xl:grid xl:grid-cols-3 xl:gap-6">
+          <div class="flex flex-col items-center max-w-4xl px-6 py-8 mx-auto mt-12 space-y-8 rounded-lg sm:py-4 xl:rounded-full dark:bg-gray-300 xl:space-y-0 xl:mt-8 xl:items-end justify-items-start xl:flex-none xl:grid xl:grid-cols-3 xl:gap-6">
             <div>
               <a href="https://www.mpg.de">
                 <img class="inline-block h-12" src="mpg-logo.svg" alt="Max-Planck-Gesellschaft">
-                <span class="text-lg">Max-Planck-Gesellschaft</span>
+                <span class="text-lg text-gray-800">Max-Planck-Gesellschaft</span>
               </a>
             </div>
             <a href="https://www.mpi-sws.org">
