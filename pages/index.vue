b866fdb17a93ce27babaa8926f8ace519f80face
Sebastian Probst Eide <sebastian@aircloak.com>
2021-03-01 11:21:58 +0100

    Add privacy policy

--- pages/index.vue
+++ pages/index.vue
@@ -53,14 +53,18 @@
 
     <div class="flex-grow"></div>
 
-    <footer class="flex-grow-0 pt-20 pb-10 mx-auto text-sm text-gray-500">
-      Copyright MPI-SWS for Software Systems |
-      <a
-        href="https://imprint.mpi-klsb.mpg.de/sws/www.open-diffix.org/"
-        class="text-gray-800 underline dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-300"
-      >
-        Imprint
-      </a>
+    <footer class="flex flex-col flex-grow-0 p-2 pt-20 pb-10 mx-auto text-sm text-gray-500">
+      <span>Copyright MPI-SWS for Software Systems</span>
+      <span class="mx-auto mt-2 space-x-2">
+        <a
+          href="https://imprint.mpi-klsb.mpg.de/sws/open-diffix.org/"
+          class="text-gray-800 underline dark:text-gray-400 hover:text-gray-500 dark:hover:text-gray-200"
+        >Imprint</a>
+        <a
+          href="https://data-protection.mpi-klsb.mpg.de/sws/open-diffix.org"
+          class="text-gray-800 underline dark:text-gray-400 hover:text-gray-500 dark:hover:text-gray-200"
+        >Privacy Policy</a>
+      </span>
     </footer>
   </div>
 </template>
