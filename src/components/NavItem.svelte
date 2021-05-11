7d53ab0b22cf0a1cbbf9c8b970f4bd839802908b
Sebastian Probst Eide <sebastian@aircloak.com>
2021-05-11 12:45:44 +0200

    Fix navitem spacing

--- src/components/NavItem.svelte
+++ src/components/NavItem.svelte
@@ -5,17 +5,17 @@
   $: currentPage = href && href === $page.path;
 </script>
 
-<div>
+<div class="font-semibold">
   {#if currentPage}
     <div
-      class="px-4 pt-4 h-full border-b-4 border-shamrock-600 bg-shamrock-400 text-white"
+      class="px-4 py-4 h-full border-b-4 border-shamrock-600 bg-shamrock-400 text-white"
     >
       <slot />
     </div>
   {:else}
     <a
       {href}
-      class="px-4 py-4 block text-gray-600 hover:text-gray-700 hover:pb-3 hover:bg-gray-100 hover:border-b-4 border-gray-200"
+      class="px-4 py-4 block text-gray-500 hover:text-gray-700 hover:pb-4 hover:bg-gray-100 hover:border-b-4 border-gray-200"
     >
       <slot />
     </a>
