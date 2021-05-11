6608459e249fb64b7464c82543dc2f487d00c60a
Sebastian Probst Eide <sebastian@aircloak.com>
2021-05-11 13:42:55 +0200

    Nav bar colour update on small screen
    Generally less obtrusive styling

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="h-auto md:h-24 transition-all duration-200">
-  <div class="md:fixed w-full border-b-2 border-collapse shadow-sm bg-gray-50">
+  <div class="md:fixed w-full border-b-2 border-collapse md:shadow-lg md:bg-gray-50 md:border-none">
     <nav class="max-w-2xl mx-auto w-full -mb-1 flex justify-end text-xl">
       <NavItem href="/">
         Home
--- src/components/NavItem.svelte
+++ src/components/NavItem.svelte
@@ -8,14 +8,14 @@
 <div class="font-semibold">
   {#if currentPage}
     <div
-      class="shadow-xl px-4 py-4 h-full border-b-4 border-shamrock-600 bg-shamrock-400 text-white"
+      class="transition-colors duration-500 shadow-md px-4 py-4 h-full border-b-4 border-pink-500 md:border-shamrock-600 md:bg-shamrock-400 bg-pink-400 text-white"
     >
       <slot />
     </div>
   {:else}
     <a
       {href}
-      class="duration-50 transition-shadow hover:shadow-sm px-4 py-4 block text-gray-500 hover:text-gray-700 hover:pb-4 hover:bg-gray-100 hover:border-b-4 border-gray-200"
+      class="duration-50 transition-shadow px-4 py-4 block text-gray-500 hover:text-gray-700 hover:pb-4 hover:bg-white-50 hover:border-b-4 border-gray-200 md:hover:border-shamrock-200 hover:border-pink-300"
     >
       <slot />
     </a>
