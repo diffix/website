09790bd19eca6139446c5d6eb3ed85fade0d2c06
Sebastian Probst Eide <sebastian@aircloak.com>
2021-05-12 11:59:45 +0200

    Run formatter

--- src/components/CopyableCommand.svelte
+++ src/components/CopyableCommand.svelte
@@ -1,31 +1,44 @@
 <script lang="ts">
   export let cmd: string;
-	let textarea;
+  let textarea;
 
-  let copied = false
+  let copied = false;
 
   function copy() {
     copied = true;
-    setInterval(() => copied = false, 3000);
-		textarea.select();
-		document.execCommand('copy');
+    setInterval(() => (copied = false), 3000);
+    textarea.select();
+    document.execCommand("copy");
   }
 </script>
 
 <div class="flex items-center mt-2 font-mono text-semibold text-sm px-2 py-1 bg-gray-100 -mx-2 rounded-md">
   <span class="flex-grow" on:dblclick={copy}>{cmd}</span>
 
-  <button class="hover:bg-shamrock-400 transition-all focus:outline-none duration-200 bg-shamrock-200 text-gray-800 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 border rounded-lg w-10 h-8 flex justify-center items-center" on:click={copy}>
+  <button
+    class="hover:bg-shamrock-400 transition-all focus:outline-none duration-200 bg-shamrock-200 text-gray-800 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 border rounded-lg w-10 h-8 flex justify-center items-center"
+    on:click={copy}
+  >
     {#if copied}
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+        />
       </svg>
     {:else}
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+        />
       </svg>
     {/if}
   </button>
 </div>
 
-<textarea class="hidden" bind:value={cmd} bind:this={textarea}></textarea>
+<textarea class="hidden" bind:value={cmd} bind:this={textarea} />
--- src/routes/demo.svelte
+++ src/routes/demo.svelte
@@ -15,40 +15,37 @@
 
   <div class="max-w-xl mx-auto mt-8">
     <p>
-      <Wordmark /> is currently under development, but we are happy to show you
-      the progress we have made to date.
+      <Wordmark /> is currently under development, but we are happy to show you the progress we have made to date.
     </p>
 
     <div class="bg-gray-50 border shadow-sm my-6 -mx-2 px-4 py-2 rounded-lg text-gray-700">
       <h2 class="text-2xl">TL;DR</h2>
 
       <p class="mt-2 text-sm">
-        If you are impatient, go ahead and connect to the anonymized PostgreSQL database from your command line
-        using:
+        If you are impatient, go ahead and connect to the anonymized PostgreSQL database from your command line using:
       </p>
       <CopyableCommand cmd="psql postgresql://banking_publish:demo@demo-pg.open-diffix.org/banking" />
     </div>
 
     <h2 class="mt-12 text-2xl">Long version</h2>
     <p class="mt-3">
-      We are hosting a PostgreSQL database instance containing a slightly altered version of the
-      20 odd year old
-      <Link href="https://data.world/lpetrocelli/czech-financial-dataset-real-anonymized-transactions">Berka bank dataset</Link>.
+      We are hosting a PostgreSQL database instance containing a slightly altered version of the 20 odd year old
+      <Link href="https://data.world/lpetrocelli/czech-financial-dataset-real-anonymized-transactions"
+        >Berka bank dataset</Link
+      >.
     </p>
 
     <p class="mt-2">
-      The PostgreSQL instance has the Open Diffix plugin installed and enabled in an early version
-      of the Publish mode. This is the mode whereby an analyst is expected not to be malicious, and
-      is assumed to also have access to the raw data (like you will too), but wishes to produce a
-      report or dashboard that is safe to share with others.
+      The PostgreSQL instance has the Open Diffix plugin installed and enabled in an early version of the Publish mode.
+      This is the mode whereby an analyst is expected not to be malicious, and is assumed to also have access to the raw
+      data (like you will too), but wishes to produce a report or dashboard that is safe to share with others.
     </p>
 
     <h2 class="mt-6 text-xl">Credentials</h2>
     <p class="mt-2">
-      You are given the credentials for two distinct PostgreSQL roles.
-      One is called <span>banking</span> and gives you un-anonymized read-only access to all the data in the database.
-      The other is called <span>banking_publish</span> and gives you access to the same tables, but with the anonymization
-      restrictions applied.
+      You are given the credentials for two distinct PostgreSQL roles. One is called <span>banking</span> and gives you
+      un-anonymized read-only access to all the data in the database. The other is called <span>banking_publish</span> and
+      gives you access to the same tables, but with the anonymization restrictions applied.
     </p>
 
     <div class="shadow rounded-lg -mx-2 p-2 mt-4">
@@ -81,7 +78,9 @@
 
     <p class="mt-10">
       If you have the
-      <Link href="https://blog.timescale.com/tutorials/how-to-install-psql-on-mac-ubuntu-debian-windows/">PostgreSQL client tools</Link>
+      <Link href="https://blog.timescale.com/tutorials/how-to-install-psql-on-mac-ubuntu-debian-windows/"
+        >PostgreSQL client tools</Link
+      >
       installed on your machine you can connect as the <span class="font-mono">banking</span> role with:
       <CopyableCommand cmd="psql postgresql://banking:demo@demo-pg.open-diffix.org/banking" />
 
@@ -93,7 +92,9 @@
 
     <h2 class="mt-12 text-2xl">Examples</h2>
     <p class="mt-2">
-      For some inspiration, you can view an example <Link href="https://github.com/diffix/demo_notebook">Python notebook here</Link>.
+      For some inspiration, you can view an example <Link href="https://github.com/diffix/demo_notebook"
+        >Python notebook here</Link
+      >.
     </p>
   </div>
 </div>
