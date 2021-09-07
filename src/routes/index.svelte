814a6f85f7b0a706690ea206fbd60cd81b2e83cf
Edon Gashi <egashi@mpi-sws.org>
2021-09-07 12:23:15 +0200

    Add demo video to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,8 +14,34 @@
   <h2 class="mx-12 mt-8 text-2xl text-center">Free and Open Dynamic Anonymization for PostgreSQL</h2>
 </header>
 
-<div class="max-w-4xl px-4 mx-auto mt-20 text-lg lg:flex">
-  <div class="flex-col justify-center max-w-lg mx-auto lg:w-1/2 lg:px-16 lg:pb-16 lg:flex">
+<div class="max-w-6xl px-4 mx-auto mt-20 text-lg lg:flex">
+  <div class="flex-col justify-center max-w-lg mx-auto lg:w-2/5 lg:pr-10 lg:pb-16 lg:flex">
+    <p>
+      The Open Diffix project aims to bring world-class dynamic anonymization to your software project, and is free to
+      use for most use cases.
+    </p>
+    <p class="mt-4 font-semibold">It's PostgreSQL as you know it - only anonymous.</p>
+  </div>
+  <div class="mx-auto mt-8 text-base lg:mt-0 lg:mx-0 lg:w-3/5">
+    <!-- svelte-ignore a11y-media-has-caption -->
+    <div class="">
+      <div class="overflow-hidden text-white bg-gray-900 rounded-lg shadow-lg dark:bg-shamrock-800">
+        <div class="flex px-2 pt-2 pb-2 transition-colors border-b border-gray-700">
+          <div class="w-3 h-3 bg-red-500 rounded-full hover:bg-red-400" />
+          <div class="w-3 h-3 ml-1 bg-yellow-500 rounded-full hover:bg-yellow-400" />
+          <div class="w-3 h-3 ml-1 bg-green-500 rounded-full hover:bg-green-400" />
+        </div>
+        <video autoplay controls muted loop>
+          <source src="static/short_demo.webm" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="max-w-6xl px-4 mx-auto mt-20 text-lg lg:flex">
+  <div class="flex-col justify-center max-w-lg mx-auto lg:w-2/5 lg:pr-10 lg:pb-16 lg:flex">
     <p>
       The Open Diffix project aims to bring world-class dynamic anonymization to your software project, and is free to
       use for most use cases.
