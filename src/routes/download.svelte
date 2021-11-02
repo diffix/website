debaa2d435e8802416c4707c5d7f49d4b375e06e
Edon Gashi <egashi@mpi-sws.org>
2021-11-02 12:52:14 +0100

    Refactor content page layout to shared component

--- src/routes/download.svelte
+++ src/routes/download.svelte
@@ -1,67 +1,58 @@
 <script>
+  import ContentPage from "../components/ContentPage.svelte";
   import Link from "../components/Link.svelte";
   import DownloadLink from "../components/DownloadLink.svelte";
 </script>
 
-<svelte:head>
-  <title>open-diffix - Download</title>
-</svelte:head>
+<ContentPage title="Download">
+  <h2 class="mt-12 text-2xl">Diffix for Desktop (Beta)</h2>
 
-<div class="p-4">
-  <header class="max-w-xl pt-12 mx-auto">
-    <h1 class="block text-5xl font-semibold">Download</h1>
-  </header>
+  <h3 class="mt-4 font-semibold text-xl">Latest version</h3>
 
-  <div class="max-w-xl mx-auto mt-8">
-    <h2 class="mt-12 text-2xl">Diffix for Desktop (Beta)</h2>
+  <ul class="mt-2 list-disc list-inside">
+    <li>
+      <DownloadLink href="#">Windows</DownloadLink>
+    </li>
+    <li>
+      <DownloadLink href="#">MacOS</DownloadLink>
+    </li>
+    <li>
+      <DownloadLink href="#">Debian</DownloadLink>
+    </li>
+  </ul>
 
-    <h3 class="mt-4 font-semibold text-xl">Latest version</h3>
+  <h3 class="mt-4 font-semibold text-xl">Previous versions</h3>
 
-    <ul class="mt-2 list-disc list-inside">
-      <li>
-        <DownloadLink href="#">Windows</DownloadLink>
-      </li>
-      <li>
-        <DownloadLink href="#">MacOS</DownloadLink>
-      </li>
-      <li>
-        <DownloadLink href="#">Debian</DownloadLink>
-      </li>
-    </ul>
+  <p class="mt-2">
+    Previous releases can be found in our <Link href="https://github.com/diffix/desktop/releases"
+      >GitHub repository</Link
+    >.
+  </p>
 
-    <h3 class="mt-4 font-semibold text-xl">Previous versions</h3>
+  <h2 class="mt-12 text-2xl">Sample Data</h2>
 
-    <p class="mt-2">
-      Previous releases can be found in our <Link href="https://github.com/diffix/desktop/releases"
-        >GitHub repository</Link
-      >.
-    </p>
+  <p class="mt-2">
+    CSV files that can be used to test Diffix for Desktop. Descriptions of tables can be found <Link
+      href="https://github.com/diffix/desktop/tree/master/sample_data">here</Link
+    >.
+  </p>
 
-    <h2 class="mt-12 text-2xl">Sample Data</h2>
+  <ul class="mt-2 list-disc list-inside">
+    <li>
+      <DownloadLink href="https://github.com/diffix/desktop/raw/master/sample_data/taxi.csv.zip"
+        >taxi.csv.zip</DownloadLink
+      >
+    </li>
+    <li>
+      <DownloadLink href="https://github.com/diffix/desktop/raw/master/sample_data/census.csv.zip"
+        >census.csv.zip</DownloadLink
+      >
+    </li>
+  </ul>
 
-    <p class="mt-2">
-      CSV files that can be used to test Diffix for Desktop. Descriptions of tables can be found <Link
-        href="https://github.com/diffix/desktop/tree/master/sample_data">here</Link
-      >.
-    </p>
+  <h2 class="mt-12 text-2xl">Diffix for PostgreSQL</h2>
 
-    <ul class="mt-2 list-disc list-inside">
-      <li>
-        <DownloadLink href="https://github.com/diffix/desktop/raw/master/sample_data/taxi.csv.zip"
-          >taxi.csv.zip</DownloadLink
-        >
-      </li>
-      <li>
-        <DownloadLink href="https://github.com/diffix/desktop/raw/master/sample_data/census.csv.zip"
-          >census.csv.zip</DownloadLink
-        >
-      </li>
-    </ul>
-
-    <h2 class="mt-12 text-2xl">Diffix for PostgreSQL</h2>
-
-    <p class="mt-2">
-      Diffix for PostgreSQL is still under development. Click <Link href="/demo">here</Link> for more details.
-    </p>
-  </div>
-</div>
+  <p class="mt-2">
+    Diffix for PostgreSQL is still under development. Click <Link href="/demo">here</Link> for more details.
+  </p>
+</ContentPage>
--- src/routes/newsletter.svelte
+++ src/routes/newsletter.svelte
@@ -1,4 +1,5 @@
 <script>
+  import ContentPage from "../components/ContentPage.svelte";
   import { trackAction } from "../ackee";
 
   function trackSubscription() {
@@ -6,115 +7,95 @@
   }
 </script>
 
-<svelte:head>
-  <title>open-diffix - Newsletter</title>
-</svelte:head>
-
-<div class="p-4">
-  <header class="max-w-xl pt-12 mx-auto">
-    <h1 class="block text-5xl font-semibold">Newsletter</h1>
-  </header>
-
-  <div class="max-w-xl mx-auto mt-8">
-    <div id="mc_embed_signup">
-      <form
-        action="https://open-diffix.us1.list-manage.com/subscribe/post?u=4006b84ec4d64e7ae6f4c8f49&amp;id=bc48f0af67"
-        method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
-        class="validate"
-        target="_blank"
-        novalidate
-        on:submit={trackSubscription}
-      >
-        <div id="mc_embed_signup_scroll">
-          <p class="mt-4">
-            Sign up to our mailing list and we will keep you up to date on the latest developments around open-diffix.
-          </p>
-          <div id="mergeRow-gdpr" class="text-sm mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
-            <div class="mt-8 text-base content__gdpr">
-              <fieldset class="mt-2 mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
-                <legend class="text-base mb-2">
-                  Please select the types of information you would like to receive:
-                </legend>
-                <label
-                  class="flex items-center px-1 -mx-1 rounded cursor-pointer checkbox subfield hover:bg-gray-100"
-                  for="gdpr_39706"
-                >
-                  <input
-                    type="checkbox"
-                    id="gdpr_39706"
-                    name="gdpr[39706]"
-                    value="Y"
-                    class="av-checkbox gdpr"
-                    checked
-                  />
-                  <span class="ml-2">Updates about the Open-Diffix project - no spam, no ads</span>
-                </label>
-                <label
-                  class="flex items-baseline px-1 mt-1 -mx-1 rounded cursor-pointer hover:bg-gray-100 checkbox subfield"
-                  for="gdpr_39710"
-                  ><input type="checkbox" id="gdpr_39710" name="gdpr[39710]" value="Y" class="av-checkbox gdpr" />
-                  <span class="ml-2"> Information from partner organizations about Open-Diffix related services </span>
-                </label>
-              </fieldset>
-            </div>
-          </div>
-          <div class="px-2 pb-2 mt-4 -mx-2 bg-gray-100 rounded-lg mc-field-group group focus-within:bg-gray-100">
-            <label for="mce-EMAIL" class="font-medium ml-1 text-xs text-gray-800">Your email address</label>
-            <div class="flex flex-col space-y-1 sm:space-y-0 sm:space-x-1 sm:flex-row">
-              <input
-                type="email"
-                value=""
-                name="EMAIL"
-                class="w-full px-2 py-1 text-lg border rounded required email focus:outline-none focus:ring-2"
-                placeholder="you@privacy-aware.com"
-                id="mce-EMAIL"
-              />
-              <input
-                type="submit"
-                value="Subscribe"
-                name="subscribe"
-                id="mc-embedded-subscribe"
-                class="px-2 py-1 border rounded cursor-pointer bg-shamrock-700 hover:bg-gradient-to-r from-shamrock-500 to-royalblue-500 text-white text-lg focus:outline-none focus:ring-2"
-                on:click={trackSubscription}
-              />
-            </div>
-          </div>
-          <div>
-            <div>
-              <p class="mt-8 text-xs text-gray-600">
-                You can unsubscribe at any time by clicking the link in the footer of our emails. For information about
-                our privacy practices, please read our
-                <a
-                  href="https://data-protection.mpi-klsb.mpg.de/sws/open-diffix.org"
-                  class="text-gray-700 underline hover:text-gray-900">privacy policy</a
-                >.
-              </p>
-            </div>
-            <div class="mt-2 text-xs text-gray-600 content__gdprLegal">
-              <p>
-                We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your
-                information will be transferred to Mailchimp for processing.
-                <a
-                  href="https://mailchimp.com/legal/"
-                  target="_blank"
-                  class="text-gray-900 underline hover:text-gray-800"
-                  >Learn more about Mailchimp's privacy practices here.</a
-                >
-              </p>
-            </div>
-          </div>
-          <div id="mce-responses" class="clear">
-            <div class="response" id="mce-error-response" style="display: none" />
-            <div class="response" id="mce-success-response" style="display: none" />
-          </div>
-          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-          <div style="position: absolute; left: -5000px" aria-hidden="true">
-            <input type="text" name="b_4006b84ec4d64e7ae6f4c8f49_bc48f0af67" tabindex="-1" value="" />
+<ContentPage title="Newsletter">
+  <div id="mc_embed_signup">
+    <form
+      action="https://open-diffix.us1.list-manage.com/subscribe/post?u=4006b84ec4d64e7ae6f4c8f49&amp;id=bc48f0af67"
+      method="post"
+      id="mc-embedded-subscribe-form"
+      name="mc-embedded-subscribe-form"
+      class="validate"
+      target="_blank"
+      novalidate
+      on:submit={trackSubscription}
+    >
+      <div id="mc_embed_signup_scroll">
+        <p class="mt-4">
+          Sign up to our mailing list and we will keep you up to date on the latest developments around open-diffix.
+        </p>
+        <div id="mergeRow-gdpr" class="text-sm mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
+          <div class="mt-8 text-base content__gdpr">
+            <fieldset class="mt-2 mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
+              <legend class="text-base mb-2">
+                Please select the types of information you would like to receive:
+              </legend>
+              <label
+                class="flex items-center px-1 -mx-1 rounded cursor-pointer checkbox subfield hover:bg-gray-100"
+                for="gdpr_39706"
+              >
+                <input type="checkbox" id="gdpr_39706" name="gdpr[39706]" value="Y" class="av-checkbox gdpr" checked />
+                <span class="ml-2">Updates about the Open-Diffix project - no spam, no ads</span>
+              </label>
+              <label
+                class="flex items-baseline px-1 mt-1 -mx-1 rounded cursor-pointer hover:bg-gray-100 checkbox subfield"
+                for="gdpr_39710"
+                ><input type="checkbox" id="gdpr_39710" name="gdpr[39710]" value="Y" class="av-checkbox gdpr" />
+                <span class="ml-2"> Information from partner organizations about Open-Diffix related services </span>
+              </label>
+            </fieldset>
           </div>
         </div>
-      </form>
-    </div>
+        <div class="px-2 pb-2 mt-4 -mx-2 bg-gray-100 rounded-lg mc-field-group group focus-within:bg-gray-100">
+          <label for="mce-EMAIL" class="font-medium ml-1 text-xs text-gray-800">Your email address</label>
+          <div class="flex flex-col space-y-1 sm:space-y-0 sm:space-x-1 sm:flex-row">
+            <input
+              type="email"
+              value=""
+              name="EMAIL"
+              class="w-full px-2 py-1 text-lg border rounded required email focus:outline-none focus:ring-2"
+              placeholder="you@privacy-aware.com"
+              id="mce-EMAIL"
+            />
+            <input
+              type="submit"
+              value="Subscribe"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              class="px-2 py-1 border rounded cursor-pointer bg-shamrock-700 hover:bg-gradient-to-r from-shamrock-500 to-royalblue-500 text-white text-lg focus:outline-none focus:ring-2"
+              on:click={trackSubscription}
+            />
+          </div>
+        </div>
+        <div>
+          <div>
+            <p class="mt-8 text-xs text-gray-600">
+              You can unsubscribe at any time by clicking the link in the footer of our emails. For information about
+              our privacy practices, please read our
+              <a
+                href="https://data-protection.mpi-klsb.mpg.de/sws/open-diffix.org"
+                class="text-gray-700 underline hover:text-gray-900">privacy policy</a
+              >.
+            </p>
+          </div>
+          <div class="mt-2 text-xs text-gray-600 content__gdprLegal">
+            <p>
+              We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your
+              information will be transferred to Mailchimp for processing.
+              <a href="https://mailchimp.com/legal/" target="_blank" class="text-gray-900 underline hover:text-gray-800"
+                >Learn more about Mailchimp's privacy practices here.</a
+              >
+            </p>
+          </div>
+        </div>
+        <div id="mce-responses" class="clear">
+          <div class="response" id="mce-error-response" style="display: none" />
+          <div class="response" id="mce-success-response" style="display: none" />
+        </div>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px" aria-hidden="true">
+          <input type="text" name="b_4006b84ec4d64e7ae6f4c8f49_bc48f0af67" tabindex="-1" value="" />
+        </div>
+      </div>
+    </form>
   </div>
-</div>
+</ContentPage>
