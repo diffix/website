5be321c387103ddd3000ea47637c6aa8594c4add
Edon Gashi <egashi@mpi-sws.org>
2021-11-02 10:21:09 +0100

    Reorder menu items

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -7,11 +7,11 @@
     <nav class="max-w-2xl mx-auto w-full -mb-1 flex justify-end text-xl">
       <NavItem href="/">Home</NavItem>
 
+      <NavItem href="/download">Download</NavItem>
+
       <NavItem href="/newsletter">Newsletter</NavItem>
 
       <NavItem href="/faq">FAQ</NavItem>
-
-      <NavItem href="/download">Download</NavItem>
     </nav>
   </div>
 </div>
