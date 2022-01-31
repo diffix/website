055327f8fdfe9cef06314352b2d011f50e66f07e
Edon Gashi <egashi@mpi-sws.org>
2022-01-31 15:07:29 +0100

    Set up blog index and page template

---
+++ src/routes/blog/_post.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let title;
+  export let date;
+  export let author;
+</script>
+
+<svelte:head>
+  <title>Open Diffix - {title}</title>
+</svelte:head>
+
+<div class="px-4 py-8">
+  <article class="break-words mx-auto prose lg:prose-lg">
+    <p style="margin-bottom: 0.5em;">Published on {date} by {author}</p>
+    <slot />
+  </article>
+</div>
